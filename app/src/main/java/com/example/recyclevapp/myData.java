53a0b9435fc94cc3c3649bd1067c6ebfa2ce4372
ofekdrihan <drihan98@gmail.com>
2024-12-20 23:47:29 +0200

    Added project files
    Added project files of my recycle view project using android studio.

---
+++ app/src/main/java/com/example/recyclevapp/myData.java
@@ -0,0 +1,23 @@
+package com.example.recyclevapp;
+
+public class myData {
+
+    static String[] nameArray = {"Spongebob", "Patrick", "Squidward", "Sandy", "Gary", "Mr Krab", "Pearl","Plankton", "Karen", "Mrs Puff"};
+    static String[] descriptionArray = {"SpongeBob is a sea sponge, innocent, passionate, sensitive, childish, with a shrill and high-pitched laugh, a raspy voice and square pants (which gave him his last name)."
+            , "Patrick is the best friend of SpongeBob, the main character in the series. Patrick is a starfish. Patrick is described as \"ignorant\" and has a short-term memory, and behaves stupidly and naively."
+            , "Squidward is an octopus, although he has six arms. Squidward is characterized as neurotic, miserable, sarcastic and narcissistic. Some also see him as a tragic figure, trying to get recognition from the environment without success."
+            , "Sandy is among the most intelligent people in Bikini Bottom, a scientist and martial artist. Since she's a squirrel and doesn't breathe underwater, she walks around in a suit that allows her to breathe."
+            , "Gary the Snail is SpongeBob's pet snail. Gary's intelligence is higher than any other character in the series. Garry has a pink shell engraved with a red stripe and five purple circles. Gary often howls like a cat."
+            , "Mr.Krab's personality is characterized by an obsession with money, great avarice and greed. In one of the episodes he even tried to murder Squidward in order to get money for his creations. Mr. Krab runs the crispy crab restaurant"
+            , "Pearl Krab is the teenage daughter of Mr.Krab. Pearl is a whale and is significantly taller than the other characters. She usually wears a pink shirt with the letter P printed on it. Pearl is often ashamed of her father, Mr.Krab."
+            , "Plankton is the main villain of the series and the sworn enemy of Mr.Krab. Plankton runs \"The Bucket Restaurant\". Since the restaurant is not profitable, he tries throughout the series to steal the secret formula of the crab meatball."
+            , "Karen is married to Plankton and is a huge screen in Plankton's lab, she is connected to many outlets. Karen used to be a calculator, but Plankton upgraded her to a computer."
+            ,"Mrs Puff owns a sailing school in Bikini Bottom, where she also serves as a driving instructor. She is SpongeBob's driving instructor, she hates him because he repeatedly fails his driving tests."};
+
+    static Integer[] drawableArray = {R.drawable.spongebob, R.drawable.patrick, R.drawable.squidward,
+            R.drawable.sandy, R.drawable.gary, R.drawable.mr_krab,
+            R.drawable.pearl, R.drawable.plankton, R.drawable.karen,R.drawable.mrs_puff};
+
+    static Integer[] id_ = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+
+}
